75defc74478a34ad2fc572f5a50d72891ad1f22b
j-sims <jim@esims.org>
2019-04-09 07:42:55 -0400

    add iiq_db start

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+/etc/init.d/iiq_db start && \
 /etc/init.d/insightiq start 2>&1 && \
 echo "Startup Successful. Wait up to 5 minutes for services."
 /usr/bin/tail -f /var/log/insightiq.log
