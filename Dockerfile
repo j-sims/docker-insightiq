f699910c653c838869718ee83339707bfb32463d
Jim Sims <jim@esims.org>
2019-04-11 08:02:42 -0400

    Added memory tuning to postgresql.conf via tunedb.sh

--- Dockerfile
+++ Dockerfile
@@ -29,5 +29,6 @@ EXPOSE 443 80
 # Adds the startup script and specifies run command
 # Docker doesn't use init service, init.d scripts are not run
 ADD run.sh /
+ADD tunedb.sh /
 ADD disable_ipv6.sh /
 CMD ["/run.sh"]
--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@ IPV6="${WITH_IPV6_DISABLED:-FALSE}"
 if [[ "$IPV6" == TRUE ]] && [[ -x /disable_ipv6.sh ]]; then
 	/disable_ipv6.sh && rm /disable_ipv6.sh
 fi
+if [[ -x /tunedb.sh ]]; then
+    /tunedb.sh && rm /tunedb.sh
+fi
 /etc/init.d/iiq_db start && \
 /etc/init.d/insightiq start 2>&1 && \
 echo "Startup Successful. Wait up to 5 minutes for services."
